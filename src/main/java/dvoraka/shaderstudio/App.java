0e1f97a8282374518adca32a28c82e7cfbfb5b33
dvoraka <alen.dvorak@gmail.com>
2015-10-25 22:11:45 +0100

    Prepare app skelet

---
+++ src/main/java/dvoraka/shaderstudio/App.java
@@ -0,0 +1,11 @@
+package dvoraka.shaderstudio;
+
+/**
+ * Created by dvoraka on 10/25/15.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Shader studio");
+    }
+}
